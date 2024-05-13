5760f5321fd19a7759c8f88ffb9c8f811f7767df
banhmisg9509 <banhmisg9509@gmail.com>
2024-05-13 19:03:11 +0700

    fix build

--- src/composables/useGetIPLookup.ts
+++
@@ -1,9 +0,0 @@
-import { getIPLookup } from '@/includes/weatherAPI'
-import { useQuery } from '@tanstack/vue-query'
-
-export const useGetIPLookup = () => {
-  return useQuery({
-    queryKey: ['ip-look-up'],
-    queryFn: () => getIPLookup()
-  })
-}
